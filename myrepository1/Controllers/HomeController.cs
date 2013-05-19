4a1aae40d18a3faece230e2062a9c4487d3b549f
ar-ar <arwa.agha@gmail.com>
2013-05-19 21:56:48 +0300

    initial commit

---
+++ myrepository1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myrepository1.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome this is my application in appharbor  arwa agha!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ myrepository1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myrepository1.MvcApplication" Language="C#" %>
